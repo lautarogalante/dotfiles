3029996c166d9d16f289b21a4496833ecb00a614
Lautaro Galante <69444294+lautarogalante@users.noreply.github.com>
2023-01-27 01:18:25 -0300

    Update config.h

--- config/dwm/config.h
+++ config/dwm/config.h
@@ -48,9 +48,9 @@ static const Layout layouts[] = {
 #define MODKEY Mod4Mask
 #define TAGKEYS(KEY, TAG)                                          \
     {MODKEY, KEY, view, {.ui = 1 << TAG}},                         \
-        {MODKEY | ControlMask, KEY, toggleview, {.ui = 1 << TAG}}, \
-        {MODKEY | ShiftMask, KEY, tag, {.ui = 1 << TAG}},          \
-        {MODKEY | ControlMask | ShiftMask, KEY, toggletag, {.ui = 1 << TAG}},
+    {MODKEY | ControlMask, KEY, toggleview, {.ui = 1 << TAG}}, \
+    {MODKEY | ShiftMask, KEY, tag, {.ui = 1 << TAG}},          \
+    {MODKEY | ControlMask | ShiftMask, KEY, toggletag, {.ui = 1 << TAG}},
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd)                                           \
@@ -70,60 +70,61 @@ static const char *mutevol[] = {"/usr/bin/pactl", "set-sink-mute", "0", "toggle"
 
 static Key keys[] = {
     /* modifier                     key        function        argument */
-    {MODKEY, XK_p, spawn, {.v = dmenucmd}},
-    {MODKEY | ShiftMask, XK_Return, spawn, {.v = termcmd}},
-    {MODKEY, XK_b, togglebar, {0}},
-    {MODKEY, XK_j, focusstack, {.i = +1}},
-    {MODKEY, XK_k, focusstack, {.i = -1}},
-    {MODKEY, XK_i, incnmaster, {.i = +1}},
-    {MODKEY, XK_d, incnmaster, {.i = -1}},
-    {MODKEY, XK_h, setmfact, {.f = -0.05}},
-    {MODKEY, XK_l, setmfact, {.f = +0.05}},
-    {MODKEY, XK_Return, zoom, {0}},
-    {MODKEY, XK_Tab, view, {0}},
-    {MODKEY, XK_q, killclient, {0}},
-    {MODKEY, XK_t, setlayout, {.v = &layouts[0]}},
-    {MODKEY, XK_f, setlayout, {.v = &layouts[1]}},
-    {MODKEY, XK_m, setlayout, {.v = &layouts[2]}},
-    {MODKEY, XK_space, setlayout, {0}},
-    {MODKEY | ShiftMask, XK_space, togglefloating, {0}},
-    {MODKEY, XK_0, view, {.ui = ~0}},
-    {MODKEY | ShiftMask, XK_0, tag, {.ui = ~0}},
-    {MODKEY, XK_comma, focusmon, {.i = -1}},
-    {MODKEY, XK_period, focusmon, {.i = +1}},
-    {MODKEY | ShiftMask, XK_comma, tagmon, {.i = -1}},
-    {MODKEY | ShiftMask, XK_period, tagmon, {.i = +1}},
+    {MODKEY, 			   XK_p, 	spawn, 		{.v = dmenucmd}},
+    {MODKEY | ShiftMask, 	   XK_Return, 	spawn, 		{.v = termcmd}},
+    {MODKEY, 			   XK_b, 	togglebar, 	{0}},
+    {MODKEY, 			   XK_j, 	focusstack, 	{.i = +1}},
+    {MODKEY, 			   XK_k, 	focusstack, 	{.i = -1}},
+    {MODKEY, 			   XK_i, 	incnmaster, 	{.i = +1}},
+    {MODKEY, 			   XK_d, 	incnmaster,     {.i = -1}},
+    {MODKEY, 			   XK_h, 	setmfact, 	{.f = -0.05}},
+    {MODKEY, 			   XK_l, 	setmfact, 	{.f = +0.05}},
+    {MODKEY, 			   XK_Return, 	zoom, 		{0}},
+    {MODKEY, 			   XK_Tab, 	view, 		{0}},
+    {MODKEY, 			   XK_q, 	killclient, 	{0}},
+    {MODKEY, 			   XK_t, 	setlayout, 	{.v = &layouts[0]}},
+    {MODKEY, 			   XK_f, 	setlayout, 	{.v = &layouts[1]}},
+    {MODKEY, 			   XK_m, 	setlayout,      {.v = &layouts[2]}},
+    {MODKEY, 			   XK_space,    setlayout,      {0}},
+    {MODKEY | ShiftMask, 	   XK_space,    togglefloating, {0}},
+    {MODKEY, 			   XK_0, 	view, 		{.ui = ~0}},
+    {MODKEY | ShiftMask, 	   XK_0, 	tag, 		{.ui = ~0}},
+    {MODKEY, 			   XK_comma, 	focusmon, 	{.i = -1}},
+    {MODKEY, 			   XK_period, 	focusmon, 	{.i = +1}},
+    {MODKEY | ShiftMask, 	   XK_comma, 	tagmon, 	{.i = -1}},
+    {MODKEY | ShiftMask, 	   XK_period, 	tagmon, 	{.i = +1}},
     TAGKEYS(XK_1, 0)
-	TAGKEYS(XK_2, 1)
+    TAGKEYS(XK_2, 1)
     TAGKEYS(XK_3, 2)
     TAGKEYS(XK_4, 3)
     TAGKEYS(XK_5, 4)
     TAGKEYS(XK_6, 5)
     TAGKEYS(XK_7, 6)
     TAGKEYS(XK_8, 7)
-    TAGKEYS(XK_9, 8){MODKEY | ShiftMask, XK_q, quit, {0}},
-    {MODKEY, XK_v, spawn, SHCMD("chromium")},
-    {MODKEY | ShiftMask, XK_m, spawn, {.v = mutevol}},
-    {MODKEY | ShiftMask, XK_u, spawn, {.v = upvol}},
-    {MODKEY | ShiftMask, XK_d, spawn, {.v = downvol}},
-    {MODKEY, XK_o, spawn, SHCMD("kitty")},
-    {MODKEY, XK_z, spawn, SHCMD("redshift -O 2400")},
-    {MODKEY, XK_x, spawn, SHCMD("redshift -x")},
-    {MODKEY, XK_n, spawn, SHCMD("thunar")}};
+    TAGKEYS(XK_9, 8)		  
+    {MODKEY | ShiftMask, 	   XK_q, 	quit, 	 	 {0}},
+    {MODKEY, 			   XK_v, 	spawn, 		SHCMD("chromium")},
+    {MODKEY | ShiftMask, 	   XK_m, 	spawn, 		{.v = mutevol}},
+    {MODKEY | ShiftMask, 	   XK_u, 	spawn, 		{.v = upvol}},
+    {MODKEY | ShiftMask, 	   XK_d, 	spawn, 		{.v = downvol}},
+    {MODKEY, 			   XK_o, 	spawn, 		SHCMD("kitty")},
+    {MODKEY, 			   XK_z, 	spawn, 		SHCMD("redshift -O 2400")},
+    {MODKEY, 			   XK_x, 	spawn, 		SHCMD("redshift -x")},
+    {MODKEY, 			   XK_n, 	spawn, 		SHCMD("thunar")}};
 
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
     /* click                event mask      button          function        argument */
-    {ClkLtSymbol, 0, Button1, setlayout, {0}},
-    {ClkLtSymbol, 0, Button3, setlayout, {.v = &layouts[2]}},
-    {ClkWinTitle, 0, Button2, zoom, {0}},
-    {ClkStatusText, 0, Button2, spawn, {.v = termcmd}},
-    {ClkClientWin, MODKEY, Button1, movemouse, {0}},
-    {ClkClientWin, MODKEY, Button2, togglefloating, {0}},
-    {ClkClientWin, MODKEY, Button3, resizemouse, {0}},
-    {ClkTagBar, 0, Button1, view, {0}},
-    {ClkTagBar, 0, Button3, toggleview, {0}},
-    {ClkTagBar, MODKEY, Button1, tag, {0}},
-    {ClkTagBar, MODKEY, Button3, toggletag, {0}},
+    {ClkLtSymbol, 		0, 	    Button1, 	    setlayout, 		{0}},
+    {ClkLtSymbol, 		0, 	    Button3, 	    setlayout, 		{.v = &layouts[2]}},
+    {ClkWinTitle, 		0, 	    Button2, 	    zoom,		{0}},
+    {ClkStatusText, 		0, 	    Button2,        spawn, 		{.v = termcmd}},
+    {ClkClientWin, 		MODKEY,     Button1, 	    movemouse, 		{0}},
+    {ClkClientWin, 		MODKEY,     Button2,        togglefloating,     {0}},
+    {ClkClientWin, 		MODKEY,     Button3,        resizemouse,        {0}},
+    {ClkTagBar, 		0,  	    Button1, 	    view, 		{0}},
+    {ClkTagBar, 		0, 	    Button3, 	    toggleview, 	{0}},
+    {ClkTagBar, 		MODKEY,     Button1, 	    tag, 		{0}},
+    {ClkTagBar, 		MODKEY,     Button3,        toggletag, 		{0}},
 };
